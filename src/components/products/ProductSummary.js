88545a193eeac3657c049a8bddf6efd9a1dffa18
harkanrand <harkaranr0@gmail.com>
2021-08-09 23:35:00 -0500

    display description

--- src/components/products/ProductSummary.js
+++ src/components/products/ProductSummary.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const ProductSummary = ({ product }) => {
   return (
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
-        <span className="card-title">{product.name}</span>
+        <span className="card-title">{product.description}</span>
       </div>
     </div>
   );
